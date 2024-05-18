3e12d708e7056adc79d6ec708078ec3bd49e0335
Artur Barbosa <arturbarbosap@gmail.com>
2024-05-18 17:45:26 -0300

    Adicionado o Algoritmo de Euclides e seu passo a passo. Alterações na interface para a melhor visualização do passo a passo.

--- AlgoritmoEstendidoEuclides/frm_algoritmo.Designer.cs
+++ AlgoritmoEstendidoEuclides/frm_algoritmo.Designer.cs
@@ -36,43 +36,45 @@
             // 
             // txt_resultado
             // 
-            txt_resultado.Location = new Point(256, 179);
+            txt_resultado.Font = new Font("Segoe UI", 18F);
+            txt_resultado.Location = new Point(23, 74);
             txt_resultado.Multiline = true;
             txt_resultado.Name = "txt_resultado";
             txt_resultado.ReadOnly = true;
-            txt_resultado.Size = new Size(288, 182);
-            txt_resultado.TabIndex = 7;
+            txt_resultado.Size = new Size(288, 364);
+            txt_resultado.TabIndex = 3;
             // 
             // btn_calcular
             // 
-            btn_calcular.Location = new Point(256, 131);
+            btn_calcular.Location = new Point(23, 41);
             btn_calcular.Name = "btn_calcular";
             btn_calcular.Size = new Size(288, 27);
-            btn_calcular.TabIndex = 6;
+            btn_calcular.TabIndex = 2;
             btn_calcular.Text = "Calcular";
             btn_calcular.UseVisualStyleBackColor = true;
+            btn_calcular.Click += btn_calcular_Click;
             // 
             // txt_numB
             // 
-            txt_numB.Location = new Point(466, 89);
+            txt_numB.Location = new Point(233, 12);
             txt_numB.Name = "txt_numB";
             txt_numB.PlaceholderText = "Número B";
             txt_numB.Size = new Size(78, 23);
-            txt_numB.TabIndex = 5;
+            txt_numB.TabIndex = 1;
             // 
             // txt_numA
             // 
-            txt_numA.Location = new Point(256, 89);
+            txt_numA.Location = new Point(23, 12);
             txt_numA.Name = "txt_numA";
             txt_numA.PlaceholderText = "Número A";
             txt_numA.Size = new Size(78, 23);
-            txt_numA.TabIndex = 4;
+            txt_numA.TabIndex = 0;
             // 
             // frm_algoritmo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(335, 450);
             Controls.Add(txt_resultado);
             Controls.Add(btn_calcular);
             Controls.Add(txt_numB);
--- AlgoritmoEstendidoEuclides/frm_algoritmo.cs
+++ AlgoritmoEstendidoEuclides/frm_algoritmo.cs
@@ -6,5 +6,29 @@ namespace AlgoritmoEstendidoEuclides
         {
             InitializeComponent();
         }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            int numa, numb, quoc, resto;
+            resto = 1;
+            string passo = $"MDC({txt_numA.Text}, {txt_numB.Text}) = \r\n"; //string do passo a passo
+
+            numa = Math.Max(int.Parse(txt_numA.Text), int.Parse(txt_numB.Text)); //o maior número entra na variável numA
+            numb = Math.Min(int.Parse(txt_numA.Text), int.Parse(txt_numB.Text)); //o menor número entra na variável numB
+
+            while (resto != 0) //enquanto o resto não for 0, continuar a divisão
+            {
+                quoc = numa / numb; //quociente dos dois números
+                resto = numa % numb; //resto da divisão, que será o divisor da próxima divisão
+
+                passo += $"{numa} = {numb} . {quoc} + {resto}\r\n"; //exibe a equação no passo a passo
+
+                numa = numb; //maior número vira o dividendo da divisão anterior
+                numb = resto; //o resto vira o divisor da próxima divisão
+            }
+            //resultado é o último resto antes do resto ser igual a 0
+            passo += $"MDC({txt_numA.Text}, {txt_numB.Text}) = {numa}"; //final do passo a passo, mostra o resultado
+            txt_resultado.Text = passo; //exibindo o resultado na tela
+        }
     }
 }
